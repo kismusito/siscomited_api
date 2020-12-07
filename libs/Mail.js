1c5ab8e5896523c2bb33b5f2d29568fb3dc2d100
Desarrollador Web <desarrolladorweb@MacBook-Pro-de-Desarrollador.local>
2020-12-07 13:51:31 -0500

    mail

--- libs/Mail.js
+++ libs/Mail.js
@@ -3,9 +3,7 @@ require("dotenv").config();
 
 async function Mail(template) {
     const transport = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 587,
-        secure: false,
+        service: "gmail",
         auth: {
             user: process.env.EMAIL,
             pass: process.env.PASSWORD,
