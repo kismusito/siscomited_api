8f3390230b9e1fe3acf625bb965098c1551c6401
Desarrollador Web <desarrolladorweb@MacBook-Pro-de-Desarrollador.local>
2020-12-07 09:47:10 -0500

    path

--- controllers/uploads/imageController.js
+++ controllers/uploads/imageController.js
@@ -13,7 +13,7 @@ function generateRamdom(n) {
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, "assets/img");
+        cb(null, path.join(__dirname, "assets/img"));
     },
     filename: function (req, file, cb) {
         cb(null, generateRamdom(40) + "-" + path.extname(file.originalname));
